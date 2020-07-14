7bf01862abb4d9b9ba6dbc3897e77b32b790c284
cbevel <chelsey.bevel@gmail.com>
2020-07-13 22:49:21 -0400

    starting the first test

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,4 +1,13 @@
 package org.launchcode.techjobs_oo.Tests;
 
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 public class JobTest {
+
+    @Test
+    public void testSettingJobId(){
+        assertEquals(10, 10, 10);
+    }
 }
+
